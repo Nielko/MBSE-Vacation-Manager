27d7ccc0ed32bf086c73f6b9111cdb66398dc5b7
anhtutran <anhtutran@192.168.0.21>
2017-02-09 19:34:18 +0100

    util- und runtime Ordner hinzugefügt

---
+++ de.tu_bs.vacation_manager.test.util/src/de/tu_bs/vacation_manager/test/util/Calender2TextUtil.java
@@ -0,0 +1,87 @@
+package de.tu_bs.vacation_manager.test.util;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import de.tu_bs.vacation_manager.Calender.Date;
+
+public class Calender2TextUtil {
+	
+	public String dateFormat (Date day) throws ParseException{
+		
+		DateFormat input = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+		DateFormat outputFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+		String inputDate = day.getDay().toString();
+		java.util.Date date = input.parse(inputDate);
+		String formattedDate = outputFormat.format(date);
+		
+		return formattedDate; 
+	
+		
+	}
+	
+	
+	public String dateFormatSingleDay (Date day) throws ParseException{
+		
+		DateFormat input = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+		DateFormat outputFormat = new SimpleDateFormat("dd", Locale.ENGLISH);
+		String inputDate = day.getDay().toString();
+		java.util.Date date = input.parse(inputDate);
+		String formattedDate = outputFormat.format(date);
+		
+		return formattedDate; 
+	
+		
+	}
+	
+	
+	public String dateFormatYear (Date day) throws ParseException{
+		
+		DateFormat input = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+		DateFormat outputFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+		String inputDate = day.getDay().toString();
+		java.util.Date date = input.parse(inputDate);
+		String formattedDate = outputFormat.format(date);
+		
+		return formattedDate; 
+	
+		
+	}
+	
+	
+	public String dateMonth (Date day) throws ParseException{
+		
+		DateFormat input = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+		DateFormat outputFormat = new SimpleDateFormat("MMM", Locale.ENGLISH);
+		String inputDate = day.getDay().toString();
+		java.util.Date date = input.parse(inputDate);
+		String formattedDate = outputFormat.format(date);
+		
+		return formattedDate; 
+	
+		
+	}
+	
+	
+	public String getDayType (Date day) {
+		
+		String dayType="";
+		
+		String inputDayType = day.getDayType().toString();	
+		if (inputDayType == "WorkingDay") 
+			dayType="Werktag"; 
+		else if (inputDayType == "Holiday")
+			dayType="Urlaub";
+		else if (inputDayType == "Vacation") 
+			dayType="Urlaub";
+		else if (inputDayType == "PublicHoliday") 
+			dayType="Feiertag";
+		
+		return dayType; 
+	
+		
+	}
+	
+
+}
