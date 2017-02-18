a385359f118753ec5eb8c9d1de1b10b441a93ae5
Niels Kowala <Niels_Kowala@web.de>
2017-02-18 12:08:18 +0100

    - Experimente mit EGL und Java (nicht funktionsfähig)

--- runtime-EclipseApplication/Vacation_Manager2Text/src/StartEGL.java
+++ runtime-EclipseApplication/Vacation_Manager2Text/src/StartEGL.java
@@ -1,21 +1,48 @@
 
+import java.io.File;
+
+import org.eclipse.epsilon.common.parse.problem.ParseProblem;
 import org.eclipse.epsilon.egl.EglFileGeneratingTemplateFactory;
 import org.eclipse.epsilon.egl.EgxModule;
+import org.eclipse.epsilon.egl.internal.EglModule;
+import org.eclipse.epsilon.emc.emf.AbstractEmfModel;
+import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.emc.plainxml.PlainXmlModel;
 
 public class StartEGL {
 
 	public static void main(String[] args) {
 		// Parse main.egx
-	    EgxModule module = new EgxModule(new EglFileGeneratingTemplateFactory());
-	    module.parse(new File("main.egx").getAbsoluteFile());
+	    EglModule module = new EglModule();
+	    try {
+			module.parse(new File("model/Calender2HTML.egl").getAbsoluteFile());
+			
+			EmfModel model = new EmfModel();
+		    model.setModelFile("/model/My.calender");
+		    model.load();
+		    
+			//module.execute(null);
+	    } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	    if (!module.getParseProblems().isEmpty()) {
-	      System.out.println("Syntax errors found. Exiting.");
-	      return;
-	    }
+	    if (module.getParseProblems().size() > 0) {
+			System.err.println("Parse errors occured...");
+			for (ParseProblem problem : module.getParseProblems()) {
+				System.err.println(problem.toString());
+			}
+			System.exit(-1);
+		}
+	    
+	    
+	    
+	   
+	    //module.execute(template, postprocessor);
+	    
+	    
 
-	    // Load the XML document
+	   /*7 // Load the XML document
 	    PlainXmlModel model = new PlainXmlModel();
 	    model.setFile(new File("library.xml"));
 	    model.setName("L");
@@ -25,7 +52,7 @@ public class StartEGL {
 	    module.getContext().getModelRepository().addModel(model);
 	    // ... and execute
 	    module.execute();
-	  }
+	  }*/
 	}
 
 }
