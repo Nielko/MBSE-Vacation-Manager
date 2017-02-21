814efad33be5afd75c09516d0e506c507939b2b8
Niels Kowala <Niels_Kowala@web.de>
2017-02-21 19:07:09 +0100

    - Generieren der Score-Instanz (Anh Tu)

---
+++ runtime-EclipseApplication/Vacation_Manager2Text/src/CalenderInstantiation.java
@@ -0,0 +1,104 @@
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.Map;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import de.tu_bs.vacation_manager.Calender.Calender;
+import de.tu_bs.vacation_manager.Calender.CalenderFactory;
+import de.tu_bs.vacation_manager.Calender.Date;
+import de.tu_bs.vacation_manager.Calender.Employee;
+import de.tu_bs.vacation_manager.Calender.WorkingDayType;
+
+public class CalenderInstantiation {
+	
+	public static void main(String[] args) throws ParseException {
+		
+		int year = 2017-1900;
+		int [] sizeMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30 , 31, 30 , 31};	
+		
+		//Überprüfung eines Schaltjahres
+		if (new GregorianCalendar().isLeapYear(year) == true)
+			sizeMonth[1] = 27;
+		
+		Calender cal = CalenderFactory.eINSTANCE.createCalender();
+		cal.setName("Vacation Manager");
+		
+		///////Fixe Feiertage
+		String [] publicHolidayDate = {"01.01.", "01.05.", "03.10", "25.12.", "26.12."};
+		String [] publicHolidays = new String [100];
+
+		//Zum Feiertag wird das aktuelle Jahr hinzugefügt
+		for (int i = 0; i<publicHolidayDate.length; i++){	
+			publicHolidays [i] = publicHolidayDate[i]+""+(year+1900);
+		}
+				
+		//Erstellung und Überprüfung der einzelnen Kalendertage
+		for (int month=0; month<=11; month++){
+			
+			for (int day=1; day<=sizeMonth[month]; day++){	
+				Date date = CalenderFactory.eINSTANCE.createDate();
+				date.setDay(new java.util.Date(year,month,day));
+				date.setName(""+date.getDay());
+				cal.getDates().add(date);
+				
+				//Funktion zum formatieren des Datum
+				DateFormat input = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+				DateFormat outputFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+				String inputDate = date.getDay().toString();
+				java.util.Date days = input.parse(inputDate);
+				String formattedDate = outputFormat.format(days);
+				
+				//Überprüfung ob das Datum ein Feiertag ist
+				for (int i = 0; i<publicHolidayDate.length; i++){	
+					if (formattedDate.compareTo(publicHolidays[i]) == 0 )	
+					date.setDayType(WorkingDayType.PUBLIC_HOLYDAY);
+				}
+				//Überprüfung ob das Datum ein Wochenende ist
+				if (date.getDay().getDay() == 0 || date.getDay().getDay() == 6 )
+					date.setDayType(WorkingDayType.PUBLIC_HOLYDAY);
+				
+			}
+		}
+		
+		String [][] employees = { 
+				{"Peter Meyer" ,"10"}, 
+				{"Max Mustermann" ,"7"}, 
+				{"Patrick Müller" ,"5"},
+				{"Anna Meyer" ,"30"}	
+				};		
+		
+		for (int i = 0; i<employees.length; i++){
+			for (int j = 0; j<employees[i].length-1; j++){
+				Employee employee = CalenderFactory.eINSTANCE.createEmployee();
+				employee.setName(employees[i][j]);
+				employee.setLeaveDays(Integer.parseInt(employees[i][j+1]));
+				cal.getPersons().add(employee);	
+			}
+		}
+			
+		
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("calender", new XMIResourceFactoryImpl());
+		
+		ResourceSet rs = new ResourceSetImpl();
+		Resource r = rs.createResource(URI.createFileURI("my.calender"));
+		r.getContents().add(cal);
+		System.out.println("Instanz erstellt");
+		
+		
+		try {
+			r.save(Collections.EMPTY_MAP);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
