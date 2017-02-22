d505eccec43541b630c318fd47a65b7671259f3c
Niels Kowala <Niels_Kowala@web.de>
2017-02-22 20:58:24 +0100

    Komplette Java-Datei für den Start von EGL

--- runtime-EclipseApplication/Vacation_Manager2Text/src/StartEGL.java
+++ runtime-EclipseApplication/Vacation_Manager2Text/src/StartEGL.java
@@ -1,38 +1,67 @@
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 import org.eclipse.epsilon.common.parse.problem.ParseProblem;
+import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.egl.EglFileGeneratingTemplateFactory;
+import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
 import org.eclipse.epsilon.egl.EgxModule;
 import org.eclipse.epsilon.egl.internal.EglModule;
 import org.eclipse.epsilon.emc.emf.AbstractEmfModel;
 import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.emc.plainxml.PlainXmlModel;
+import org.eclipse.epsilon.eol.IEolExecutableModule;
+import org.eclipse.epsilon.eol.models.IRelativePathResolver;
 
 public class StartEGL {
 
 	public static void main(String[] args) {
 		// Parse main.egx
-	    EglModule module = new EglModule();
 	    try {
-			module.parse(new File("model/Calender2HTML.egl").getAbsoluteFile());
-			
-			EmfModel model = new EmfModel();
-		    model.setModelFile("/model/My.calender");
-		    model.load();
-		    
-			//module.execute(null);
-	    } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	    if (module.getParseProblems().size() > 0) {
+	    	// Egl-Datei laden und Output setzen
+	    	EglFileGeneratingTemplateFactory eglTemplateFactory = new EglFileGeneratingTemplateFactory();
+	    IEolExecutableModule module = new EglTemplateFactoryModuleAdapter(eglTemplateFactory);
+	    	System.out.println("Parsing template files...");
+	    eglTemplateFactory.setOutputRoot("model/calender.html");
+	    	module.parse(new File("model/Calender2HTML.egl").getAbsoluteFile());
+	
+	    	// Fehler beim Parsen ausgeben
+	    	if (module.getParseProblems().size() > 0) {
 			System.err.println("Parse errors occured...");
 			for (ParseProblem problem : module.getParseProblems()) {
 				System.err.println(problem.toString());
 			}
-			System.exit(-1);
+		}
+	    	
+	    	// Metamodell hinzufügen
+	    	EmfModel model = new EmfModel();
+	    	StringProperties properties = new StringProperties();
+	    properties.put(EmfModel.PROPERTY_NAME, "Mein Kalender");
+	    properties.put(EmfModel.PROPERTY_FILE_BASED_METAMODEL_URI, "model/Calender.ecore");//"file:/" + new File(metamodel).getAbsolutePath());
+	    	properties.put(EmfModel.PROPERTY_MODEL_URI, "file:/" + new File("model/mynew.calender").getAbsolutePath());
+	    properties.put(EmfModel.PROPERTY_IS_METAMODEL_FILE_BASED, "true");
+	    properties.put(EmfModel.PROPERTY_READONLOAD, true + "");
+	    	properties.put(EmfModel.PROPERTY_STOREONDISPOSAL, true + "");
+	    model.load(properties, (IRelativePathResolver) null);
+	    eglTemplateFactory.getContext().getModelRepository().addModel(model) ;
+	    	
+	    
+	    	// Ausführen und in Datei schreiben
+	    System.out.println(	module.execute());
+	    File file = new File("model/calender.html");
+	    FileOutputStream fop = new FileOutputStream(file);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		byte[] contentInBytes = ((String) module.execute()).getBytes();
+		fop.write(contentInBytes);
+		fop.flush();
+		fop.close();
+	    	module.getContext().getModelRepository().dispose();
+	    } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	    
 	    
