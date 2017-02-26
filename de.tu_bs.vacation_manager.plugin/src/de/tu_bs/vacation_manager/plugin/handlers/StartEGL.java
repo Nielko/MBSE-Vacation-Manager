6d4c925b4ea0fbdc3e0afaafbf3c31089900ce4f
Niels Kowala <Niels_Kowala@web.de>
2017-02-26 11:55:27 +0100

    Starten einer Jar im Plugin

--- de.tu_bs.vacation_manager.plugin/src/de/tu_bs/vacation_manager/plugin/handlers/StartEGL.java
+++ de.tu_bs.vacation_manager.plugin/src/de/tu_bs/vacation_manager/plugin/handlers/StartEGL.java
@@ -21,45 +21,17 @@ public class StartEGL {
 	public static void start(String project) {
 		// Parse main.egx
 	    try {
+	    	
+	    	/*Process ps=Runtime.getRuntime().exec(new String[]{"java -jar .."+project+"/StartEgl.jar"});
+	        ps.waitFor();
+	        System.out.println("Jar gestartet");
+	        java.io.InputStream is=ps.getInputStream();
+	        byte b[]=new byte[is.available()];
+	        is.read(b,0,b.length);
+	        System.out.println(new String(b));*/
 	    	// Egl-Datei laden und Output setzen
-	    	EglFileGeneratingTemplateFactory eglTemplateFactory = new EglFileGeneratingTemplateFactory();
-	    IEolExecutableModule module = new EglTemplateFactoryModuleAdapter(eglTemplateFactory);
-	    	System.out.println("Parsing template files...");
-	    	/*module.parse(new File(project+"/model/Calender2HTML.egl").getAbsoluteFile());
-	
-	    	// Fehler beim Parsen ausgeben
-	    	if (module.getParseProblems().size() > 0) {
-			System.err.println("Parse errors occured...");
-			for (ParseProblem problem : module.getParseProblems()) {
-				System.err.println(problem.toString());
-			}
-		}
+	    	de.mbse.runegl.StartEGL.start("file://"+project);
 	    	
-	    	// Metamodell hinzufügen
-	    	EmfModel model = new EmfModel();
-	    	StringProperties properties = new StringProperties();
-	    properties.put(EmfModel.PROPERTY_NAME, "Mein Kalender");
-	    properties.put(EmfModel.PROPERTY_FILE_BASED_METAMODEL_URI,project+ "/model/Calender.ecore");//"file:/" + new File(metamodel).getAbsolutePath());
-	    	properties.put(EmfModel.PROPERTY_MODEL_URI, "file:/" + new File(project+"/model/mynew.calender").getAbsolutePath());
-	    properties.put(EmfModel.PROPERTY_IS_METAMODEL_FILE_BASED, "true");
-	    properties.put(EmfModel.PROPERTY_READONLOAD, true + "");
-	    	properties.put(EmfModel.PROPERTY_STOREONDISPOSAL, true + "");
-	    model.load(properties, (IRelativePathResolver) null);
-	    eglTemplateFactory.getContext().getModelRepository().addModel(model) ;
-	    	
-	    
-	    	// Ausführen und in Datei schreiben
-	    System.out.println(	module.execute());
-	    File file = new File(project+"/model/calender.html");
-	    FileOutputStream fop = new FileOutputStream(file);
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-		byte[] contentInBytes = ((String) module.execute()).getBytes();
-		fop.write(contentInBytes);
-		fop.flush();
-		fop.close();
-	    	module.getContext().getModelRepository().dispose();*/
 	    } catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- runtime-EclipseApplication/Vacation_Manager2Text/src/de/mbse/runegl/Validate.java
+++ runtime-EclipseApplication/Vacation_Manager2Text/src/de/mbse/runegl/Validate.java
@@ -1,5 +1,30 @@
 package de.mbse.runegl;
 
-public class Validate {
+import java.util.Iterator;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+public class Validate {
+	public Validate()
+	{
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+	    Map<String, Object> m = reg.getExtensionToFactoryMap();
+	    m.put("calender", new XMIResourceFactoryImpl());
+	    
+
+		ResourceSet rs = new ResourceSetImpl();
+		Resource r = rs.getResource(URI.createFileURI("model/mynew.calender"), true);
+		
+		EObject root = r.getContents().get(0);
+		
+		Iterator<EObject> i = r.getAllContents();
+		while(i.hasNext())
+			System.out.println(i.next());
+	}
 }
