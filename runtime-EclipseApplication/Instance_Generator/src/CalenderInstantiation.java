113c9165dfb13d24e8deb45c071e9ed95a3326f6
anhtutran <anhtutran@w1208.wlan.rz.tu-bs.de>
2017-02-27 14:46:14 +0100

    KW

--- runtime-EclipseApplication/Instance_Generator/src/CalenderInstantiation.java
+++ runtime-EclipseApplication/Instance_Generator/src/CalenderInstantiation.java
@@ -2,8 +2,10 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -21,6 +23,7 @@ import de.tu_bs.vacation_manager.Calender.Employee;
 import de.tu_bs.vacation_manager.Calender.Month;
 import de.tu_bs.vacation_manager.Calender.MonthName;
 import de.tu_bs.vacation_manager.Calender.WorkingDayType;
+import de.tu_bs.vacation_manager.Calender.Year;
 import de.tu_bs.vacation_manager.Calender.KW;
 
 public class CalenderInstantiation {
@@ -35,9 +38,7 @@ public class CalenderInstantiation {
 			sizeMonth[1] = 27;
 		
 		Calender cal = CalenderFactory.eINSTANCE.createCalender();
-		cal.setName("Vacation Manager");
-
-		
+		cal.setName("Vacation Manager");	
 		
 		///////Fixe Feiertage
 		String [] publicHolidayDate = {"01.01.", "01.05.", "03.10", "25.12.", "26.12."};
@@ -47,8 +48,33 @@ public class CalenderInstantiation {
 		for (int i = 0; i<publicHolidayDate.length; i++){	
 			publicHolidays [i] = publicHolidayDate[i]+""+(year+1900);
 		}
-				
-		//Erstellung und Überprüfung der einzelnen Kalendertage
+		
+		//Kalenderwochen erstellen
+		//KW kw1 = CalenderFactory.eINSTANCE.createKW();
+		
+		
+KW kw5 = CalenderFactory.eINSTANCE.createKW();
+		
+		//Erstellung Monate
+		Year years = CalenderFactory.eINSTANCE.createYear();
+		years.setYear(year+1900);
+		cal.getYears().add(years);
+		for (int monate=0; monate<=11; monate++){;
+			Month month = CalenderFactory.eINSTANCE.createMonth();
+			month.setMonthname(MonthName.values()[monate]);
+			years.getMonths().add(month);
+		}
+		
+		for (int i=1; i<=52; i++){
+
+			KW kw = CalenderFactory.eINSTANCE.createKW();
+			kw.setKw(i);
+			cal.getKws().add(kw);
+		}
+		int weekNumber  = -1;
+		
+	//Erstellung und Überprüfung der einzelnen Kalendertage
+		//for (int woche = 1; woche <=52; woche++){
 		for (int month=0; month<=11; month++){
 			
 			for (int day=1; day<=sizeMonth[month]; day++){	
@@ -64,6 +90,26 @@ public class CalenderInstantiation {
 				java.util.Date days = input.parse(inputDate);
 				String formattedDate = outputFormat.format(days);
 				
+				
+				DateFormat outputFormatWeek = new SimpleDateFormat("w", Locale.ENGLISH);
+				String inputDateWeek = date.getDay().toString();
+				java.util.Date week = input.parse(inputDateWeek);
+				String formattedDateWeek = outputFormatWeek.format(week);
+				System.out.println(formattedDateWeek);
+				
+				// Tag einer Kalenderwoche hinzufügen
+				if(date.getDay().getDay() == 1 && weekNumber < 51)
+				{
+					weekNumber++;
+					cal.getYears().get(0).getMonths().get(month).getKws().add(cal.getKws().get(weekNumber));
+				}
+				if(weekNumber >= 0 && day == 1 && date.getDay().getDay() != 1)
+					cal.getYears().get(0).getMonths().get(month).getKws().add(cal.getKws().get(weekNumber));
+				if(weekNumber >= 0)
+					cal.getKws().get(weekNumber).getWeekdates().add(date);
+				
+				
+				
 				//Überprüfung ob das Datum ein Wochenende ist
 				if (date.getDay().getDay() == 0 || date.getDay().getDay() == 6 )
 					date.setDayType(WorkingDayType.RESTRICTED_DAY);
@@ -72,26 +118,62 @@ public class CalenderInstantiation {
 				for (int i = 0; i<publicHolidayDate.length; i++){	
 					if (formattedDate.compareTo(publicHolidays[i]) == 0 )	
 					date.setDayType(WorkingDayType.PUBLIC_HOLYDAY);
-				}				
+
+				}
+
+
 			}
+			/*for (int i=0; i<=51; i++){
+			KW kw = CalenderFactory.eINSTANCE.createKW();
+				if (Integer.parseInt(formattedDateWeek) == i){
+					System.out.println(i +" ist gleich " + formattedDateWeek);
+					kw.setKw(i);
+					kw.getWeekdates().add(date);
+					System.out.println(kw.getWeekdates());
+					cal.getKws().add(i, kw);	
+					
+				
+				}
+			}*/
+
+			
 		}
+		/*Date date = CalenderFactory.eINSTANCE.createDate();
+		new java.util.Date(year,3,21);
+		DateFormat input = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+		DateFormat outputFormat = new SimpleDateFormat("w", Locale.ENGLISH);
+		String inputDate = date.getDay().toString();
+		java.util.Date days = input.parse(inputDate);
+		String formattedDate = outputFormat.format(days);
+		System.out.println(formattedDate);
+*/
+
 		
-		//Erstellung Monate
-	/*	String [] monthName = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};	
-		for (int monate=0; monate<=monthName.length; monate++){
-			Month month = CalenderFactory.eINSTANCE.createMonth();
-			month.setMonthname(MonthName.values()[monate]);
-			cal.getMonth().add(month);
-			cal.get
+		
+		
+	/*	for (int i=0; i<=51; i++){
+			KW kw = CalenderFactory.eINSTANCE.createKW();
+			if (Integer.parseInt(formattedDate) == i){
+				System.out.println(i +" ist gleich " + formattedDate);
+				kw.setKw(i);
+				kw.getWeekdates().add(date);
+				System.out.println(kw.getWeekdates());
+				//kw.getWeekdates().set(i, date);
+				cal.getKws().add(i, kw);	
+				//cal.getKws().set(i, kw);
+			
+			}
 		}*/
-		
+
 		
 		//Kalenderwochen
-		for (int i=0; i<=52; i++){
-		KW kw = CalenderFactory.eINSTANCE.createKW();
-		kw.setKw(i);
-		cal.getKws().add(kw);
-		}
+	//	for (int i=1; i<=51; i++){
+	//	System.out.println(cal.getKws().get(i));
+	//	}
+		
+		
+
+		
 		
 		String [][] employees = { 
 				{"Peter Meyer" ,"10"}, 
@@ -108,6 +190,7 @@ public class CalenderInstantiation {
 				cal.getPersons().add(employee);	
 			}
 		}
+		
 			
 		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
 		Map<String, Object> m = reg.getExtensionToFactoryMap();
@@ -124,7 +207,6 @@ public class CalenderInstantiation {
 			System.out.println(child.getMessage());
 		
 		System.out.println("Instanz erstellt");
-		
 		
 		try {
 			r.save(Collections.EMPTY_MAP);
